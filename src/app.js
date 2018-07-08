f68c301d1adcb634f8d10aaa293643f275ea6f82
t00masz <38228974+t00masz@users.noreply.github.com>
2018-07-08 17:18:52 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
 import AppContainer from './reactApp'
 
 ReactDOM.render(
